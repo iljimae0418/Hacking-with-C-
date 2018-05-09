15e3263971a3fea155133af61af0f0addf49f894
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-05-09 02:17:20 -0700

    Update realloc_example.c

--- realloc_example.c
+++ realloc_example.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h> 
 #include <stdlib.h> 
+#include <assert.h> 
 int main(){
 	int size = 2; 
 	char **arr = (char **)malloc(size*sizeof(char *));  
+	assert(arr != NULL);  
 	arr[0] = "beef"; 
 	arr[1] = "lamb";  
 	char *temp[size]; 
@@ -11,6 +13,7 @@ int main(){
 		temp[i] = arr[i];  
 	}
 	arr = (char **)realloc(arr,2*size*sizeof(char *)); 
+	assert(arr != NULL);  
 	for (int i = 0; i < size; i++){
 		arr[i] = temp[i];  
 	}
